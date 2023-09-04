8a2361c7b0835e3378e18a9602a4ee0f2502ec1b
Rik Svendsen Rose <rik.s.rose@maersk.com>
2023-09-04 13:31:30 +0200

    Add DI to test session Id

--- src/Sample.Worker/Program.cs
+++ src/Sample.Worker/Program.cs
@@ -28,7 +28,15 @@ namespace Sample.Worker
                         x.SetKebabCaseEndpointNameFormatter();
 
                         x.AddConsumer<SubmitOrderConsumer>();
-                        x.AddConsumer<OrderSubmittedConsumer>();
+                        x.AddConsumer<OrderSubmittedConsumer>(w =>
+                        {
+                            const int partitionCount = 3;
+                            w.UsePartitioner(new Partitioner(partitionCount, new Murmur3UnsafeHashGenerator()),
+                                m => m.CorrelationId ?? Guid.NewGuid());
+                            w.UseRateLimit(3, TimeSpan.FromSeconds(5)); //max for consumer
+                            w.UseTimeout(x => x.Timeout = TimeSpan.FromMinutes(5));
+                            w.ConcurrentMessageLimit = partitionCount;
+                        });
 
                         x.AddSagaStateMachine<OrderShipmentStateMachine, OrderShipmentState, OrderShipmentSagaDefinition>()
                             .MessageSessionRepository();
@@ -45,10 +53,21 @@ namespace Sample.Worker
                             // Subscribe to OrderSubmitted directly on the topic, instead of configuring a queue
                             cfg.SubscriptionEndpoint<OrderSubmitted>("order-submitted-consumer", e =>
                             {
+                                e.ConfigureConsumeTopology = false;
+                                e.AutoStart = true;
+                                e.AutoDeleteOnIdle = TimeSpan.FromDays(30);
+                                e.MaxDeliveryCount = 3;
+                                e.RequiresSession = true;
+                                e.ConcurrentMessageLimit = 64;
+                                e.PrefetchCount = 0;
                                 e.ConfigureConsumer<OrderSubmittedConsumer>(context);
+
                             });
 
-                            cfg.ConfigureEndpoints(context);
+                            //cfg.ConfigureEndpoints(context);
+                            cfg.UseRateLimit(500, TimeSpan.FromMinutes(1)); // max for bus
+                            cfg.UseConcurrencyLimit(1); // does nothing
+                            cfg.PrefetchCount = 0;
                         });
                     });
                 });
