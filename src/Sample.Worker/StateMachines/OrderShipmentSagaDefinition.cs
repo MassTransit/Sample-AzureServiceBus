7690ef3b52adbcd3b3e12298338bec0ecc81cbf8
Ramon Smits <ramon.smits@gmail.com>
2024-06-20 13:57:51 +0200

    📦 MassTransit.Azure.ServiceBus.Core 8.2.3

--- src/Sample.Worker/StateMachines/OrderShipmentSagaDefinition.cs
+++ src/Sample.Worker/StateMachines/OrderShipmentSagaDefinition.cs
@@ -2,11 +2,14 @@ namespace Sample.Worker.StateMachines
 {
     using MassTransit;
 
-
     public class OrderShipmentSagaDefinition :
         SagaDefinition<OrderShipmentState>
     {
-        protected override void ConfigureSaga(IReceiveEndpointConfigurator endpointConfigurator, ISagaConfigurator<OrderShipmentState> sagaConfigurator)
+        protected override void ConfigureSaga(
+            IReceiveEndpointConfigurator endpointConfigurator,
+            ISagaConfigurator<OrderShipmentState> sagaConfigurator,
+            IRegistrationContext context
+            )
         {
             if (endpointConfigurator is IServiceBusReceiveEndpointConfigurator sb)
                 sb.RequiresSession = true;
